4fa47ba3ebc013f799d12dca82cdab0a9b1c7e43
KIMBEOBWOO <qjqdn1568@naver.com>
2023-07-04 21:44:57 +0900

    ✨  #10 setRepository unit test fix

--- src/test/modules/transaction.module.spec.ts
+++ src/test/modules/transaction.module.spec.ts
@@ -1,16 +1,23 @@
+import { FactoryProvider } from '@nestjs/common';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { BaseRepository } from '../../base.repository';
 import { CustomTransactionRepository } from '../../decorators/custom-transaction-repository.decorator';
 import { TransactionModule } from '../../modules/transaciton.module';
 import { ALS_SERVICE } from '../../symbols/als-service.symbol';
+import * as RepositoryUtil from '../../utils/is-base-repository-prototype';
 import { User } from '../fixture/entities/user.entity';
 import { UserV1Service } from '../fixture/services/user.v1.service';
 
 describe('TransactionModule', () => {
+  beforeEach(() => jest.restoreAllMocks());
+
   describe('setRepository', () => {
     it('In the case of a Custom Repository class that inherits the Base Repository, a useFactory that injects the Transaction Repository through metadata applied with the Custom Transaction Repository decoder must be configured.', async () => {
       @CustomTransactionRepository(User, 'Test Repository Token')
-      class TestRepository extends BaseRepository<User> {}
+      class TestRepository {}
+
+      jest
+        .spyOn(RepositoryUtil, 'isBaseRepositoryPrototype')
+        .mockReturnValue(true);
 
       const dynamicModule = TransactionModule.setRepository([TestRepository]);
 
@@ -22,6 +29,32 @@ describe('TransactionModule', () => {
         },
       ]);
       expect(dynamicModule.exports).toStrictEqual(['Test Repository Token']);
+      expect(
+        (dynamicModule.providers?.[0] as FactoryProvider).useFactory(),
+      ).toStrictEqual(new TestRepository());
+    });
+
+    it('In the case of a Custom Repository class that inherits the Base Repository, a useFactory that injects the Transaction Repository through metadata applied with the Custom Transaction Repository decoder must be configured. (Without repository token)', async () => {
+      @CustomTransactionRepository(User)
+      class TestRepository {}
+
+      jest
+        .spyOn(RepositoryUtil, 'isBaseRepositoryPrototype')
+        .mockReturnValue(true);
+
+      const dynamicModule = TransactionModule.setRepository([TestRepository]);
+
+      expect(dynamicModule.providers).toStrictEqual([
+        {
+          provide: TestRepository,
+          useFactory: expect.any(Function),
+          inject: [ALS_SERVICE],
+        },
+      ]);
+      expect(dynamicModule.exports).toStrictEqual([TestRepository]);
+      expect(
+        (dynamicModule.providers?.[0] as FactoryProvider).useFactory(),
+      ).toStrictEqual(new TestRepository());
     });
 
     it('For TypeORM Entities classes, a useFactory that injects TransactionRepository through TypeORM Repository Token must be configured.', async () => {
